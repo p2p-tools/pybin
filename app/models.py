2eda196d929984c45ece96b143322a78a1067f17
mark <lopkophacked@protonmail.com>
2024-07-06 18:47:34 +0000

    WIP

--- app/models.py
+++ app/models.py
@@ -14,12 +14,10 @@ from app import db
 class User(UserMixin, db.Model):
     """User model for db"""
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     password_hash: so.Mapped[str] = so.mapped_column(sa.String(256))
 
-    posts: so.WriteOnlyMapped['Paste'] = so.relationship(
-        back_populates='author')
+    pastes = db.relationship('Paste', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -32,16 +30,21 @@ class User(UserMixin, db.Model):
 
 
 class Paste(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default="uuid_generate_v4()")
+    files = db.relationship('File', backref='paste')
+
+    user_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[Optional[User]] = so.relationship(back_populates='posts')
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+
+
+class File(db.Model):
     """Paste model for db"""
     id = db.Column(UUID(as_uuid=True), primary_key=True, default="uuid_generate_v4()")
     filename: so.Mapped[str] = so.mapped_column(default='')
     value: so.Mapped[str] = so.mapped_column()
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey(User.id),
-                                                         index=True)
 
-    author: so.Mapped[Optional[User]] = so.relationship(back_populates='posts')
+    paste = db.Column(db.Column(UUID(as_uuid=True)), db.ForeignKey('paste.id'), nullable=False)
 
     def __repr__(self):
         return (f'<Paste(id={self.id}, value={self.value}, timestamp={self.timestamp}'
