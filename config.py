f4824cb5188f8469bdab084008174090f4bfa154
mark <lopkophacked@protonmail.com>
2024-08-22 14:19:10 +0000

    add env variables and use them in config

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = config.get('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    ENCRYPTION_KEY = config.get("ENC_KEY", "_sOncN8Fv-Rri6otHmLqOSmLw9rTXPOv-w_nJji03xI=")
